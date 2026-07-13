a45b8a5494ab669bc3dc0b2e060d33a3394f5902
ayan2424 <ayanshamim24@gmail.com>
2026-07-13 05:16:37 +0500

    Fix Employee model relationships

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,5 +8,15 @@ class Employee extends Model
 {
     protected $fillable = ['user_id', 'department_id', 'designation', 'base_salary', 'hire_date', 'is_active'];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
     //
 }
